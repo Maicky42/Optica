3319d99eeef951f3b9b17986642dee5660df625d
Makonda Daniel <150517576+Maicky42@users.noreply.github.com>
2023-12-15 16:23:37 +0100

    subi a minha pagina

---
+++ script.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal({ reset: true })
+
+sr.reveal('.slogan', {duration: 1000})
+sr.reveal('.box1', {
+    duration: 1000
+})
+sr.reveal('.box2', {duration: 1000})
+sr.reveal('.box3', {duration: 1000})
+sr.reveal('.box4', {duration: 1000})
+sr.reveal('.box5', {duration: 1000})
+sr.reveal('.box6', {duration: 1000})
+sr.reveal('.box7', {duration: 1000})
+sr.reveal('.box8', {duration: 1000})
